5350f83cd903cf4d5d902b453917ecca6206e3e7
Michael Smith <m@michaelsmith.be>
2020-04-22 00:14:12 +0200

    Bumped MiSTer and menu binary versions.

--- MiSTer-sd-installer-macos.sh
+++ MiSTer-sd-installer-macos.sh
@@ -26,8 +26,8 @@ DOWNLOAD_DIRECTORY=./download
 # TODO(m): Remove hardcoded versions.
 # URLs
 RELEASE_URL='https://github.com/MiSTer-devel/SD-Installer-Win64_MiSTer/raw/master/release_20200122.rar'
-RECENT_MISTER_URL='https://github.com/MiSTer-devel/Main_MiSTer/raw/master/releases/MiSTer_20200308'
-RECENT_MENU_MISTER_URL='https://github.com/MiSTer-devel/Menu_MiSTer/raw/master/releases/menu_20200115.rbf'
+RECENT_MISTER_URL='https://github.com/MiSTer-devel/Main_MiSTer/raw/master/releases/MiSTer_20200421'
+RECENT_MENU_MISTER_URL='https://github.com/MiSTer-devel/Menu_MiSTer/raw/master/releases/menu_20200325.rbf'
 UPDATER_SCRIPT_URL='https://raw.githubusercontent.com/MiSTer-devel/Updater_script_MiSTer/master/update.sh'
 
 # Sanity checks
